36badbcd739e69a76510f61b3acb11c7b58f3e7e
Christoph Pater <pater@adesso-mobile.de>
2019-09-08 20:34:50 +0200

    initial code sample

---
+++ src/main/java/de/ams/kata/GameOfLife.java
@@ -0,0 +1,9 @@
+package de.ams.kata;
+
+public class GameOfLife
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Game of Life" );
+    }
+}
---
+++ src/test/java/de/ams/kata/GameOfLifeTest.java
@@ -0,0 +1,14 @@
+package de.ams.kata;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class GameOfLifeTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
